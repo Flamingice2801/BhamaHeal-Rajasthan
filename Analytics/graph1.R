cdc56daf725aa6fead34ba7b027919e9038cf235
kartiksharma <98kartik.sharma@gmail.com>
2017-03-19 17:00:21 +0530

    Added first graph

---
+++ Analytics/graph1.R
@@ -0,0 +1,5 @@
+#Bar Graph for Disease Vs Number of People Affected
+library(ggplot2)
+graphone<-read.csv("ajmer.csv",TRUE,",")
+class(graphone)
+ggplot(data=graphone, aes(x=Disease, y=No.of.People.Affected)) + geom_bar(stat="identity")
