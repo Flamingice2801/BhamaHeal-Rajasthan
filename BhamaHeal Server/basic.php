0970876902b9286c74e88e8ad380ff06f81e41b6
ronaksakhuja@gmail.com <ronaksakhuja@gmail.com>
2017-03-19 15:03:43 +0530

    Added backend php

---
+++ BhamaHeal Server/basic.php
@@ -0,0 +1,19 @@
+<?php
+$bhID=$_GET['bhid'];
+$aadhar=$_GET['aadhar'];
+$name=$_GET['name'];
+
+$host="localhost";
+$uname="root";
+$pass="root";
+$DB_NAME="BhamaHeal";
+
+$conn=new mysqli($host,$uname,$pass,$DB_NAME);
+
+$query="INSERT INTO basic(Bhamashah,aadhar,name) VALUES(\"".$bhID."\",\"".$aadhar."\",\"".$name."\")";
+
+echo $query;
+
+$result=$conn->query($query);
+
+?>
