ebc67bf46b4ef8d932f2fe8da07b03d4749081a6
kartiksharma <98kartik.sharma@gmail.com>
2017-03-19 17:13:39 +0530

    updated graph1

--- Analytics/graph1.R
+++ Analytics/graph1.R
@@ -2,4 +2,4 @@
 library(ggplot2)
 graphone<-read.csv("ajmer.csv",TRUE,",")
 class(graphone)
-ggplot(data=graphone, aes(x=Disease, y=No.of.People.Affected)) + geom_bar(stat="identity")
+ggplot(data=graphone, aes(x=Disease, y=No.of.People.Affected)) + geom_bar(stat="identity",width = 0.5,fill = "#00ab57")
